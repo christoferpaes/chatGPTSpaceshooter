9c8c4c1d5d3d9acdb1463c60437b9c3c92a90f6f
Christofer Patrick Paes <49449786+christoferpaes@users.noreply.github.com>
2023-05-24 18:58:44 -0700

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -43,31 +43,19 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+  super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         // Initialize the game view
-        gameView = new GameView(this);
-        setContentView(gameView);
+        gameView = findViewById(R.id.game_view);
 
-        // Start the game
-        startGame();
+        // Start the game when the game view is clicked
+        gameView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startGame();
+            }
+        });
     }
 
     @Override
